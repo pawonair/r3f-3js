7461042a4fa81ed7065425b9bf4cbacfc9691bc3
Pawan Sunuwar <pawanman56@gmail.com>
2026-04-16 17:25:44 +0200

    add mesh

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,8 +1,10 @@
 export default function Experience() {
     return <>
-        <mesh>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
+        <mesh rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
+            {/* <sphereGeometry args={ [1.5, 32, 32] } /> */}
+            {/* <meshBasicMaterial args={ [{ color: 'red', wireframe: true }] } /> */}
+            <boxGeometry scale={ 1.5 } />
+            <meshBasicMaterial color="mediumpurple" wireframe />
         </mesh>
     </>
 }
