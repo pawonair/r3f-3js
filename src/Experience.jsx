302b12e3048ad02f6df56f2cdbedee0c47578426
Pawan Sunuwar <pawanman56@gmail.com>
2026-04-16 18:30:32 +0200

    add lights

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -17,21 +17,24 @@ export default function Experience() {
     return <>
         <orbitControls args={ [camera, gl.domElement] } />
 
+        <directionalLight position={ [1, 2, 3] } intensity={ 2.5 } />
+        <ambientLight intensity={ 1.5 } />
+
         <group ref={ groupRef }>    
             <mesh position-x={ -2 }>
                 <sphereGeometry />
-                <meshBasicMaterial color="orange" />
+                <meshStandardMaterial color="orange" />
             </mesh>
 
             <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
                 <boxGeometry scale={ 1.5 } />
-                <meshBasicMaterial color="mediumpurple" />
+                <meshStandardMaterial color="mediumpurple" />
             </mesh>
         </group>
 
         <mesh position-y={ -1 } rotation-x={ -Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshBasicMaterial color="greenyellow" />
+            <meshStandardMaterial color="greenyellow" />
         </mesh>
     </>
 }
