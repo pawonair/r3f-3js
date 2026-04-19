568477f314a82ebd3dc3058d5306f2755efb8930
Pawan Sunuwar <pawanman56@gmail.com>
2026-04-19 22:39:28 +0200

    add drei - Orbit/TransformControls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,52 +1,29 @@
-import { extend, useFrame, useThree } from "@react-three/fiber"
+import { OrbitControls, TransformControls } from "@react-three/drei"
 import { useRef } from "react"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-// import CustomObject from "./CustomObject"
-
-extend({ OrbitControls })
 
 export default function Experience() {
-    const cubeRef = useRef()
-    const groupRef = useRef()
-    const { camera, gl } = useThree()
-
-    useFrame((state, delta) => {
-        // console.log(state.clock.elapsedTime)
-        // console.log(state.clock.getElapsedTime())
-
-        // // Moving camera in circle around objects
-        // const angle = state.clock.elapsedTime
-        // state.camera.position.x = Math.sin(angle) * 8
-        // state.camera.position.z = Math.cos(angle) * 8
-        // state.camera.lookAt(0, 0, 0)
-        
-        cubeRef.current.rotation.y += delta
-        // groupRef.current.rotation.y += delta
-    })
+    const cube = useRef()
 
     return <>
-        <orbitControls args={ [camera, gl.domElement] } />
+        <OrbitControls makeDefault />
 
         <directionalLight position={ [1, 2, 3] } intensity={ 2.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <group ref={ groupRef }>    
-            <mesh position-x={ -2 }>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh>
+        <mesh position-x={ -2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
 
-            <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.23 } position-x={ 2 } scale={ 1.5 }>
-                <boxGeometry scale={ 1.5 } />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
-        </group>
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry scale={ 1.5 } />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+        <TransformControls object={ cube } mode="translate" />
 
         <mesh position-y={ -1 } rotation-x={ -Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
-        
-        {/* <CustomObject /> */}
     </>
 }
