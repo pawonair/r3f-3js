b20f4cca35746fcaaf8785a0494733d5cb84f2d7
Pawan Sunuwar <pawanman56@gmail.com>
2026-05-04 23:20:23 +0200

    debug setup

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,77 +1,28 @@
-import {
-    Float,
-    Html,
-    MeshReflectorMaterial,
-    OrbitControls,
-    PivotControls,
-    Text,
-    TransformControls
-} from "@react-three/drei"
-import { useRef } from "react"
+import { OrbitControls } from '@react-three/drei'
 
-export default function Experience() {
-    const cube = useRef()
-    const sphere = useRef()
-
+export default function Experience()
+{
     return <>
+
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [1, 2, 3] } intensity={ 2.5 } />
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <PivotControls
-            anchor={ [0, 0, 0] }
-            depthTest={ false }
-            lineWidth={ 2 }
-            axisColors={ ['#9381ff', '#ff4d6d', '#7ae582'] }
-            scale={ 100 }
-            fixed={ true }
-        >
-            <mesh ref={ sphere } position-x={ -2 }>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-                
-                <Html
-                    position={ [1, 1, 0] }
-                    wrapperClass="label"
-                    center
-                    distanceFactor={ 8 }
-                    occlude={ [sphere, cube] }
-                >
-                    This is a sphere 🟠
-                </Html>
-            </mesh>
-        </PivotControls>
+        <mesh position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry scale={ 1.5 } />
+        <mesh position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
-        <TransformControls object={ cube } mode="translate" />
 
-        <mesh position-y={ -1 } rotation-x={ -Math.PI * 0.5 } scale={ 10 }>
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            {/* <meshStandardMaterial color="greenyellow" /> */}
-            <MeshReflectorMaterial
-                resolution={ 512 }
-                blur={ [1000, 1000] }
-                mixBlur={ 1 }
-                mirror={ 0.5 }
-                color="greenyellow"
-            />
+            <meshStandardMaterial color="greenyellow" />
         </mesh>
 
-        <Float speed={ 5 } floatIntensity={ 2 }>
-            <Text
-                font="./bangers-v20-latin-regular.woff"
-                fontSize={ 1 }
-                color="salmon"
-                position-y={ 2 }
-                maxWidth={ 2 }
-                textAlign="center"
-            >
-                I LOVE R3F
-            </Text>
-        </Float>
     </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,17 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import Experience from './Experience'
-import * as THREE from 'three'
+import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
-        // dpr={ [1, 2] }
-        gl={ {
-            antialias: true,
-            toneMapping: THREE.ACESFilmicToneMapping,
-            outputColorSpace: THREE.SRGBColorSpace
-        } }
         camera={ {
             fov: 45,
-            near: 0.5,
+            near: 0.1,
             far: 200,
-            position: [3, 2, 6]
+            position: [ - 4, 3, 6 ]
         } }
     >
         <Experience />
