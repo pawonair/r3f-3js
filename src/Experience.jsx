ff8ecf17ef652386db93baeb98a1159567d8f975
Pawan Sunuwar <pawanman56@gmail.com>
2026-06-11 14:29:04 +0200

    add Suspense

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,21 +1,11 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { Suspense } from 'react'
+import Model from './Model.jsx'
+import Placeholder from './Placeholder.jsx'
 
 export default function Experience()
 {
-    const model = useLoader(
-        GLTFLoader,
-        './hamburger-draco.glb',
-        (loader) => {
-            const dracoLoader = new DRACOLoader()
-            dracoLoader.setDecoderPath('./draco/')
-            loader.setDRACOLoader(dracoLoader)
-        }
-    )
-
     return <>
 
         <Perf position="top-left" />
@@ -30,7 +20,9 @@ export default function Experience()
             <meshStandardMaterial color="greenyellow" />
         </mesh>
 
-        <primitive object={ model.scene } scale={ 0.35 } />
+        <Suspense fallback={ <Placeholder position-y={ 0.5 } scale={ [ 2, 3, 2 ] } /> } >
+            <Model />
+        </Suspense>
 
     </>
 }
