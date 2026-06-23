5682dea11f680bf0ecbdb7ffd14e8411f2dfd3a9
Pawan Sunuwar <pawanman56@gmail.com>
2026-06-23 17:12:08 +0200

    material + matcap optimization

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,8 +1,26 @@
 import { Center, OrbitControls, Text3D, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useEffect, useState } from 'react'
+import * as THREE from 'three'
+
+// ThreeJS nativ optimization
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience()
 {
+    // // Internal optimization
+    // const [ torusGeometry, setTorusGeometry ] = useState()
+    // const [ material, setMaterial ] = useState()
+
+    useEffect(() => {
+        matcapTexture.encoding = THREE.SRGBColorSpace
+        matcapTexture.needsUpdate = true
+
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
     const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
 
     return <>
@@ -11,25 +29,31 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
+        {/* <torusGeometry ref={ setTorusGeometry } args={ [1, 0.6, 16, 32] } />
+        <meshMatcapMaterial ref={ setMaterial } matcap={ matcapTexture } /> */}
+
         <Center>
-            <Text3D font="./fonts/helvetiker_regular.typeface.json"
-                    size={ 0.74 }
-                    height={ 0.2 }
-                    curveSegments={ 12 }
-                    bevelEnabled
-                    bevelThickness={ 0.02 }
-                    bevelSize={ 0.02 }
-                    bevelOffset={ 0 }
-                    bevelSegments={ 5 }
+            <Text3D
+                material={ material }
+                font="./fonts/helvetiker_regular.typeface.json"
+                size={ 0.74 }
+                height={ 0.2 }
+                curveSegments={ 12 }
+                bevelEnabled
+                bevelThickness={ 0.02 }
+                bevelSize={ 0.02 }
+                bevelOffset={ 0 }
+                bevelSegments={ 5 }
             >
                 HELLO R3F
-                <meshMatcapMaterial matcap={ matcapTexture } />
             </Text3D>
         </Center>
 
         {[...Array(100)].map((val, i) => 
             <mesh
                 key={i}
+                geometry={ torusGeometry }
+                material={ material }
                 position={[
                     (Math.random() - 0.5) * 10,
                     (Math.random() - 0.5) * 10,
@@ -41,10 +65,7 @@ export default function Experience()
                     Math.random() * Math.PI,
                     0
                 ]}
-            >
-                <torusGeometry args={ [1, 0.6, 16, 32] } />
-                <meshMatcapMaterial matcap={ matcapTexture } />
-            </mesh>
+            />
         )}
 
     </>
