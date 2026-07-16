663dfe53d95b1afdc8bd056e9d50bcf8796699cf
Pawan Sunuwar <pawanman56@gmail.com>
2026-07-16 18:46:52 +0200

    add pointer events

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,23 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import * as THREE from 'three'
 import Experience from './Experience.jsx'
-import { StrictMode } from 'react'
-import { Leva } from 'leva'
+import { Bvh } from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-const created = ({ gl, scene }) => {
-    // gl.setClearColor('#ff0000', 1)
-    scene.background = new THREE.Color('red')
-}
-
 root.render(
-    <StrictMode>
-        <Leva collapsed />
-        <Canvas
-            shadows={ false }
-            camera={ {
-                fov: 45,
-                near: 0.1,
-                far: 200,
-                position: [ - 4, 3, 6 ]
-            } }
-            // onCreated={ created }
-        >
-            {/* <color args={[ '#ff0000' ]} attach="background" /> */}
+    <Canvas
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ - 4, 3, 6 ]
+        } }
+        onPointerMissed={ () => console.log('You miss the mesh') }
+    >
+        <Bvh>
             <Experience />
-        </Canvas>
-    </StrictMode>
+        </Bvh>
+    </Canvas>
 )
